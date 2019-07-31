449ff65f050bc5474afb5cf4c257f00fff03b933
Maryanne Heinbaugh <23heinbau@gmail.com>
2019-07-30 17:18:46 -0700

    Updates

---
+++ precache-manifest.47f4d96600bb030f82c233a6c9b4c954.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91e9d1d99de37ac1526c85c59ce48415",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "fe18b5af303a4aebfe6e",
+    "url": "/clickygame/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/clickygame/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "fe18b5af303a4aebfe6e",
+    "url": "/clickygame/static/js/main.4cbba3e5.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  }
+]);
